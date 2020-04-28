bb53ef14dd32bf80854c6f4b911d78e0fa98bce1
hello9geg <1147516839@qq.com>
2020-04-28 17:28:59 +0800

    第一次提交Demo项目

---
+++ src/main/java/com/boot/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.boot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 武帅
+ * @date 2020/4/28 17:12
+ * @description
+ */
+@RestController
+public class DemoController {
+
+
+    @RequestMapping("info")
+    public String info(){
+        return "hello";
+    }
+
+
+
+}
